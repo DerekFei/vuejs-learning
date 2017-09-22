6f847a39518b5810eb613ff75cc76ba10482a091
Cheng Fei <Cheng.Fei@Honeywell.com>
2017-09-22 08:43:55 -0400

    initial commit

---
+++ The Monster Slayer/app.js
@@ -0,0 +1,89 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        turns:[]
+    },
+    methods:{
+        startGame: function(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+        attack: function(){
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits for '+ damage
+            });
+            if (this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
+
+        },
+        specialAttack: function(){
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits hard for '+ damage
+            });
+            if (this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
+
+        },
+        monsterAttacks: function(){
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Mon hits for '+ damage
+            });
+        },
+        heal: function(){
+            if(this.playerHealth <= 90){
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heal for 10'
+            });
+            this.monsterAttacks();
+        },
+        giveUp: function(){
+            this.gameIsRunning = false;
+        },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random()*max)+1, min);
+        },
+        checkWin: function(){
+            if(this.monsterHealth <= 0) {
+                if(confirm('You Won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return;
+            }else if (this.playerHealth <=0 ){
+                if(confirm('you lost! new Game?')){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+    }
+})
